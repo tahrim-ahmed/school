f2ee73eb0b39fd6284435543a462e7d9f3ea822c
sarwar_shuvo <sarwarshuvo10@gmail.com>
2023-05-08 22:14:42 +0600

    - new commit

--- dashboard.php
+++ dashboard.php
@@ -2,7 +2,7 @@
 include_once 'sys/config.php';
 include_once 'sys/database.php';
 
-$query = "SELECT * FROM teacher";
+$query = "SELECT * FROM `teacher`";
 $result = mysqli_query($link, $query);
 
 
