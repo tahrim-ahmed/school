5b823895c98327e6d7663f280655d6d92ae06d75
sarwar_shuvo <sarwarshuvo10@gmail.com>
2023-05-09 16:11:31 +0600

    view Record page done

--- records.php
+++ records.php
@@ -23,80 +23,110 @@ $result = mysqli_query($link, $query);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" type="text/css" href="<?=base_url('property/vendors/autocomplete/style.css')?>">
-    <link rel="stylesheet" type="text/css" href="<?=base_url('property/vendors/datatables/dataTables.bootstrap4.min.css')?>">
-    <link rel="stylesheet" type="text/css" href="<?=base_url('property/vendors/select2/select2.min.css')?>">
+    <link rel="stylesheet" type="text/css" href="<?= base_url('property/vendors/autocomplete/style.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?= base_url('property/vendors/datatables/dataTables.bootstrap4.min.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?= base_url('property/vendors/select2/select2.min.css') ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <!-- Bootstrap  -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>Document</title>
 
-    <script src="<?=base_url('property/vendors/jquery.min.js')?>"></script>
-    <script src="<?=base_url('property/vendors/popper.min.js')?>"></script>
-    <script src="<?=base_url('property/vendors/bootstrap/js/bootstrap.min.js')?>"></script>
-    <script src="<?=base_url('property/vendors/daterangePicker/moment.min.js')?>"></script>
+    <script src="<?= base_url('property/vendors/jquery.min.js') ?>"></script>
+    <script src="<?= base_url('property/vendors/popper.min.js') ?>"></script>
+    <script src="<?= base_url('property/vendors/bootstrap/js/bootstrap.min.js') ?>"></script>
+    <script src="<?= base_url('property/vendors/daterangePicker/moment.min.js') ?>"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-    <script src="<?=base_url('property/vendors/daterangePicker/daterangepicker.js')?>"></script>
-    <script src="<?=base_url('property/vendors/datatables/datatables.min.js')?>"></script>
-    <script src="<?=base_url('property/vendors/select2/select2.full.min.js')?>"></script>
-    <script src="<?=base_url('property/js/scripts.js')?>"></script>
-    <script src="<?=base_url('property/vendors/bootstrap/js/jqBootstrapValidation.js')?>"></script>
+    <script src="<?= base_url('property/vendors/daterangePicker/daterangepicker.js') ?>"></script>
+    <script src="<?= base_url('property/vendors/datatables/datatables.min.js') ?>"></script>
+    <script src="<?= base_url('property/vendors/select2/select2.full.min.js') ?>"></script>
+    <script src="<?= base_url('property/js/scripts.js') ?>"></script>
+    <script src="<?= base_url('property/vendors/bootstrap/js/jqBootstrapValidation.js') ?>"></script>
 </head>
 
 <body>
 <img src="./image/cover.jpg" id="background-img">
-<section class="container-fluid bkg w-75 text-white pb-5">
+<section class="container-fluid bkg w-75 mt-5 text-white pb-5">
 
     <!-- Menu Button  -->
-    <div class=" mx-auto pt-3 mt-5">
-        <div class="padding-left-5 p-4 d-flex  justify-content-around">
-            <button onclick="window.location.href = 'index.php';" type="button " class="button1 px-5 py-1 fw-bold">Home</button>
-            <button onclick="window.location.href = 'students.php';" type="button" class="button1 px-5 fw-bold">View Students</button>
+    <div class=" mx-auto pt-3">
+        <div class="padding-left-5 p-4 d-flex justify-content-around">
+            <button onclick="window.location.href = 'index.php';" type="button" class="button1 fw-bold">Home</button>
+            <div class="dropdown">
+                <button class="dropdown-toggle button1 fw-bold" type="button" id="dropdownMenuButton" data-bs-toggle='dropdown' aria-haspopup="true" aria-expanded="false">
+                    View Students
+                </button>
+                <div class="dropdown-menu p-0" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item button1 fw-bold" href="students.php">Class One</a>
+                </div>
+            </div>
+            <!--            <button onclick="window.location.href = 'students.php';" type="button" class="button1  fw-bold">View Students</button>-->
             <button type="button" class="button1 px-5 fw-bold">Notifications</button>
-            <button type="button" class="button1 px-5 fw-bold">Settings</button>
+            <div class="dropdown">
+                <button class="dropdown-toggle button1 fw-bold" type="button" id="dropdownMenuButton" data-bs-toggle='dropdown' aria-haspopup="true" aria-expanded="false">
+                    Settings
+                </button>
+                <div class="dropdown-menu p-0" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item button1 fw-bold" href="#">Teacher Name</a>
+                    <a class="dropdown-item button1 fw-bold" href="#">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="d-flex align-items-center justify-content-between px-3">
+        <div class="d-flex align-items-center justify-content-center">
+            <h4 class="pb-4">Student Record</h4>
+        </div>
+        <div class="dropdown">
+            <button class="dropdown-toggle rounded px-3 p-2 mb-3 fw-bold" type="button" id="dropdownMenuButton" data-bs-toggle='dropdown' aria-haspopup="true" aria-expanded="false">
+                Select Class
+            </button>
+            <div class="dropdown-menu p-0" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item button1 fw-bold" href="students.php">Class One</a>
+            </div>
         </div>
     </div>
 
     <!-- Student Details  -->
     <div class="d-flex align-items-center  justify-content-around">
-        <table id="student_data" class="table">
-                <thead style="color: black">
-                <tr>
-                    <th>▢</th>
-                    <th>First Name</th>
-                    <th>Surname</th>
-                    <th>Attendance</th>
-                    <th>Result</th>
-                    <th>Total</th>
-                    <th>Action</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php
-                while ($row = mysqli_fetch_array($result)) {
-                    ?>
-                    <tr>
-                        <td style="color: <?= ($row["attendance"] + $row["result"])<40 ? 'red' : 'green' ?>;">■</td>
-                        <td style="color: black"><?= $row["first_name"] ?></td>
-                        <td style="color: black"><?= $row["sur_name"] ?></td>
-                        <td style="color: black"><?= $row["attendance"] ?></td>
-                        <td style="color: black"><?= $row["result"] ?></td>
-                        <td style="color: black"><?= $row["attendance"] + $row["result"] ?></td>
-                        <td>
-                            <button class="button1 px-2 py-1 fw-bold"><i class="fa fa-pencil"></i></button>
-                        </td>
-                    </tr>
-                    <?php
-                }
+        <table id="student_data" class="table table-striped bg-light rounded p-1">
+            <thead style="color: black">
+            <tr class="px-2">
+                <th class="px-2 text-center">▢</th>
+                <th class="px-2 text-center">First Name</th>
+                <th class="px-2 text-center">Surname</th>
+                <th class="px-2 text-center">Attendance</th>
+                <th class="px-2 text-center">Result</th>
+                <th class="px-2 text-center">Total</th>
+                <th class="px-2 text-center">Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            while ($row = mysqli_fetch_array($result)) {
                 ?>
-                </tbody>
-                <tfoot>
                 <tr>
-                    
+                    <td class="px-2 text-center" style="color: <?= ($row["attendance"] + $row["result"]) < 40 ? 'red' : 'green' ?>;">■</td>
+                    <td class="px-2 text-center"><?= $row["first_name"] ?></td>
+                    <td class="px-2 text-center"><?= $row["sur_name"] ?></td>
+                    <td class="px-2 text-center"><?= $row["attendance"] ?></td>
+                    <td class="px-2 text-center"><?= $row["result"] ?></td>
+                    <td class="px-2 text-center"><?= $row["attendance"] + $row["result"] ?></td>
+                    <td>
+                        <button class="button1 px-2 py-1 fw-bold"><i class="fa fa-pencil"></i></button>
+                    </td>
                 </tr>
-                </tfoot>
-            </table>
+                <?php
+            }
+            ?>
+            </tbody>
+            <tfoot>
+            <tr>
+
+            </tr>
+            </tfoot>
+        </table>
     </div>
 </section>
 
@@ -104,53 +134,53 @@ $result = mysqli_query($link, $query);
 </body>
 
 <script>
-    $(document).ready(function () {
+    $(document).ready(function() {
         var Table = $('#student_data').DataTable({
             'bServerSide': false,
             'ordering': false,
             dom: '<"row"<"col"B><"col-auto"f>>rt<"row"<"col"i><"col-auto"l>>p',
-            buttons: [
-                {
-                    extend: 'collection',
-                    text: 'Export',
-                    buttons: [
-                        {
-                            extend: 'csv',
-                            text: 'Export as Csv',
-                            exportOptions: {
-                                columns: ':visible'
-                            }
-                        },
-                        {
-                            extend: 'excel',
-                            text: 'Export as Excel',
-                            exportOptions: {
-                                columns: ':visible'
-                            }
-                        },
-                        {
-                            extend: 'pdf',
-                            text: 'Export as PDF',
-                            exportOptions: {
-                                columns: ':visible'
-                            }
-                        },
-                        {
-                            extend: 'print',
-                            text: 'Print',
-                            exportOptions: {
-                                columns: ':visible'
-                            }
-                        }
-                    ]
+            buttons: [{
+                extend: 'collection',
+                text: 'Export',
+                buttons: [{
+                    extend: 'csv',
+                    text: 'Export as Csv',
+                    exportOptions: {
+                        columns: ':visible'
+                    }
                 },
-            ],
+                    {
+                        extend: 'excel',
+                        text: 'Export as Excel',
+                        exportOptions: {
+                            columns: ':visible'
+                        }
+                    },
+                    {
+                        extend: 'pdf',
+                        text: 'Export as PDF',
+                        exportOptions: {
+                            columns: ':visible'
+                        }
+                    },
+                    {
+                        extend: 'print',
+                        text: 'Print',
+                        exportOptions: {
+                            columns: ':visible'
+                        }
+                    }
+                ]
+            }, ],
         });
     });
 </script>
 
 <style>
-    .select2-container, .select2-container .select2-selection, .select2-container .select2-selection__rendered, .select2-container .select2-selection__arrow {
+    .select2-container,
+    .select2-container .select2-selection,
+    .select2-container .select2-selection__rendered,
+    .select2-container .select2-selection__arrow {
         height: 38px !important;
         line-height: 38px !important;
     }
--- students.php
+++ students.php
@@ -179,7 +179,7 @@ $result = mysqli_query($link, $query);
                         </div>
 
 
-<!--                        Delete Student Modal -->
+                        <!--                        Delete Student Modal -->
                         <div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div class="modal-dialog" role="document">
                                 <div class="modal-content">
