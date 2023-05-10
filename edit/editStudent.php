3cdfdddcc40002bdce9c76d0f5bd3fb95d2e5911
Tahrim Ahmed <tamiad1618@gmail.com>
2023-05-11 01:22:38 +0600

    - edit student fixed

--- edit/editStudent.php
+++ edit/editStudent.php
@@ -9,7 +9,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
     $update_sur_name = $_REQUEST['update_sur_name'];
     $update_date_of_birth = $_REQUEST['update_date_of_birth'];
 
-    $update_student_sql = "UPDATE student SET first_name = '$update_first_name', sur_name = '$update_sur_name', date_of_birth = 'update_date_of_birth' WHERE student_id = '$update_id'";
+    $update_student_sql = "UPDATE student SET first_name = '$update_first_name', sur_name = '$update_sur_name', date_of_birth = '$update_date_of_birth' WHERE student_id = '$update_id'";
 
     if ($link->query($update_student_sql)) {
 
